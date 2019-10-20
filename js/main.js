3cef315e817e514a87249dba4ac911fc32189596
FoxicUA <aleks.lisunov@gmail.com>
2019-10-20 21:10:47 +0300

    initial commit dz11

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$( function() {
+    $( "#tabs" ).tabs();
+    $( "#tabs-gallery" ).tabs();
+    $('.slider').slick({
+    dots: true,
+    arrows: true,
+});
+    $('.section-open').on('click', function(e) {
+        e.preventDefault();
+        $('.section-open').parents('.section-bg').addClass('open');
+    });
+    $('.btn-toggle').on('click', function(bt) {
+        bt.preventDefault();
+        $(this).parents('nav').addClass('isActive');
+    })
+});
+
+
+
